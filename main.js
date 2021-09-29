21f3bc46bb071db66754701d03b8dd72dbcafab0
Ramandeep <ramank6238299@gmail.com>
2021-09-29 23:41:15 +0530

    final Body

---
+++ main.js
@@ -0,0 +1,34 @@
+const dateOfBirth = document.querySelector("#date-of-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
+
+const outputBox = document.querySelector("#output-box");
+
+function compareValues(sum, luckyNumber) {
+  if (sum % luckyNumber === 0) {
+    outputBox.innerText = "Your Birthday is Lucky😎";
+  } else {
+    outputBox.innerText = "Your birthady is not lucky 🤦‍♀️";
+  }
+}
+
+function checkNumberIsLucky() {
+  const dob = dateOfBirth.value;
+  const sum = calculateSum(dob);
+  if (sum && dob) {
+    compareValues(sum, luckyNumber.value);
+  } else {
+    outputBox.innerText = "Pls enter both feilds";
+  }
+}
+
+function calculateSum(dob) {
+  dob = dob.replaceAll("-", "");
+  let sum = 0;
+  for (let index = 0; index < dob.length; index++) {
+    sum = sum + Number(dob.charAt(index));
+  }
+  return sum;
+}
+
+checkNumberButton.addEventListener("click", checkNumberIsLucky);
